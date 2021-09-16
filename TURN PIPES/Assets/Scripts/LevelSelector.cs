ff373087d884755a4743430f07c42c335bce98e2
Pixanu <quikyboy2@gmail.com>
2021-09-16 21:23:07 +0100

    Menus
    16/09/2021 - AI

---
+++ TURN PIPES/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public void Level1()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level_2");
+    }
+
+    public void Level3()
+    {
+        //SceneManager.LoadScene("Level_2");
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
